3704bc9f78c087ee1828ccc54d8a0b3accd26acb
Manas <manasarora038@gmail.com>
2021-10-18 16:27:39 +0530

    Few minor adjustments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,9 @@ import AlertTemplate from 'react-alert-template-basic';
 import App from "./app";
 
 const options = {
-    // you can also just use 'bottom center'
     position: positions.TOP_CENTER,
     timeout: 2500,
     offset: '30px',
-    // you can also just use 'scale'
     transition: transitions.FADE
 }  
 
